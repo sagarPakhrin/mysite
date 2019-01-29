1ec0e082014a909f2c7cd4d88d386022fcc7453e
Sagar Lama <sagarlama826@gmail.com>
2019-01-29 14:05:00 +0545

    Views and urls finished part 5 finished

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+from django.utils import timezone
 from blog.models import Post,Comment
 from blog.forms import PostForm,CommentForm
 #the reverse_lazy is to wait until the blog is deleted
@@ -47,3 +48,42 @@ class DraftListView(LoginRequiredMixin,ListView):
 
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+
+####################################################
+####################################################
+@login_required
+def add_comment_to_post(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post=post
+            comment.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = COmmentForm()
+        return render(request,'blog/comment_form.html',{'form':form})
+
+
+@login_required
+def comment_approve(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    return redirect('post_detail',pk=comment.post.pk)
+    comment.approve()
+
+
+@login_required
+def comment_remove(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail',pk=post_pk)
+
+
+@login_required
+ef post_publish(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.publish
+    return redirect('post_detail',pk=pk)
